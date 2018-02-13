07516843c181bd641ffdac89b887383470850d3d
Jose Medina <fermed28@gmail.com>
2018-02-12 22:26:46 -0500

    reader file

--- reto5.rb
+++ reto5.rb
@@ -3,10 +3,36 @@ class Pregunta
 
 	def initialize (question, answer)
 		@question= question
-		@answer= answer
+		@answer= answer		
 	end
 
 	def iscorrect(answer)		
 		self.answer == answer ? true : false        
 	end	
 end
+
+class Game
+	def initialize (intentos)
+		@intentos=intentos
+		@preguntas= readerFile()		
+		start()
+	end
+
+	def readerFile
+		preguntas =[]
+		File.open('Preguntas.csv', 'r') do |f1|
+  			while line = f1.gets  
+  				preguntas <<Pregunta.new(line.split(";")[0],line.split(";")[1])   			
+  			end  
+		end
+		preguntas  
+	end
+
+	def start
+		@preguntas.each do |pregunta|
+			
+		end
+	end
+end
+
+juego = Game.new(4)
